b63b398c80674568d7a8ffeac9fa4feb3bfdb576
Katherine Godwin <kgodwin019@gmail.com>
2021-04-02 23:47:16 -0400

    initial commit of all files

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from "react";
+import './App.css';
+import EightBall from './eightball';
+
+function App() {
+  return (
+    <div className="App">
+      <EightBall />
+    </div>
+  );
+}
+
+export default App;
